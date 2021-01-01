2b4aaaa00f9d8cfbcb0ad925bd4d6e36e89fbd6a
unknown <mohamedamrrefaay@gmail.com>
2021-01-02 01:21:41 +0200

    first-commitw

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,36 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ReactComponent as Upwork } from "./upwork-tile.svg";
 
 class Footer extends Component {
   render() {
-
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url}>
+              <i className={network.className}></i>
+            </a>
+          </li>
+        );
+      });
     }
 
     return (
       <footer>
-
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
               {networks}
-           </ul>
+              <li>
+                <a href="https://www.upwork.com/freelancers/~01c232f69a1e510d22">
+                  <Upwork width={"20px"} />
+                </a>
+              </li>
+            </ul>
 
-           <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
-           </ul>
-
-           <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
-
+            <ul className="copyright">
+              <li>
+                &copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker
+              </li>
+              <li>
+                Design by{" "}
+                <a title="Styleshout" href="http://www.styleshout.com/">
+                  Styleshout
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
